c8e09dfdc05ccb713920ad8545d3c04949cb826e
Mabarik667f <jc.bob1412@gmail.com>
2024-06-01 20:14:34 +0300

    feature: add asgi.settings and create JWTAUTH middleware

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -9,8 +9,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from chats import routing
+from users.middleware import JWTAuthMiddleWareStack
+
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': JWTAuthMiddleWareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )}
+)
