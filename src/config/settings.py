2cdfdcbca814e1595cc45a773a84cf0db21cc214
Mabarik667f <jc.bob1412@gmail.com>
2024-06-13 00:57:59 +0300

    settings: add cache settings, celery

--- src/config/settings.py
+++ src/config/settings.py
@@ -83,7 +83,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 ASGI_APPLICATION = 'config.asgi.application'
 
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379"
+    }
+}
 CHANNEL_LAYERS = {
     'default': {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
