9f451cf80bc024dfcbc2b4cf2e0072f656414a92
Mabarik667f <jc.bob1412@gmail.com>
2024-06-13 22:47:45 +0300

    fix: add DEFAULT_PARSER_CLASSES

--- src/config/settings.py
+++ src/config/settings.py
@@ -211,7 +211,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.FormParser',
+    ],
 }
 
 AUTH_USER_MODEL = "users.CustomUser"
